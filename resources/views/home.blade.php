4637832dbdca013986d9a9206b7fd1456ff56f7b
Mateusz Kacperski <kacperskimateusz95@gmail.com>
2024-03-11 17:11:15 +0100

    base ex

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,65 +11,28 @@
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <ul class="nav justify-content-center">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Active</a>
+              <a class="nav-link active" aria-current="page" href="#">Movie</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+              <a class="nav-link disabled" aria-disabled="true">About</a>
             </li>
           </ul>
       </nav>
       <div class="container my-4">
         <div class="row">
+            @foreach ($movies as $movie)
             <div class="col">
-                <div class="card my-4" style="width: 18rem;">
+                <div class="card my-4">
                     <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="card-link">Card link</a>
-                      <a href="#" class="card-link">Another link</a>
+                      <h5 class="card-title">{{ $movie->title }}</h5>
+                      <h6 class="card-subtitle mb-2 text-body-secondary">{{ $movie->original_title }}</h6>
+                      <p><strong>Nationality:</strong> {{ $movie->nationality }}</p>
+                      <p><strong>Date:</strong> {{ $movie->date }}</p>
+                      <p><strong>Vote:</strong> {{ $movie->vote }}</p>
                     </div>
                   </div>
             </div>
-            <div class="col">
-                <div class="card my-4" style="width: 18rem;">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="card-link">Card link</a>
-                      <a href="#" class="card-link">Another link</a>
-                    </div>
-                  </div>
-            </div>
-            <div class="col">
-                <div class="card my-4" style="width: 18rem;">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="card-link">Card link</a>
-                      <a href="#" class="card-link">Another link</a>
-                    </div>
-                  </div>
-            </div>
-            <div class="col">
-                <div class="card my-4" style="width: 18rem;">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="card-link">Card link</a>
-                      <a href="#" class="card-link">Another link</a>
-                    </div>
-                  </div>
-            </div>
+            @endforeach
         </div>
       </div>
       <footer>
